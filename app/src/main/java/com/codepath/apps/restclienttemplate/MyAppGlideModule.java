5843a79e8e459b6a746719ecd36b1e723a5293e3
tejusunkara <jnanatejaswini@gmail.com>
2021-02-16 19:49:43 -0500

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/MyAppGlideModule.java
@@ -0,0 +1,9 @@
+package com.codepath.apps.restclienttemplate;
+
+import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.annotation.GlideModule;
+
+@GlideModule
+public final class MyAppGlideModule extends AppGlideModule {
+    //empty
+}
